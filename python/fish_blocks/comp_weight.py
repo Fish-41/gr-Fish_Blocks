0af715bd8f508abad697f4f62189fbf66d6490dd
Fish-41 <oliverfishwork@gmail.com>
2024-12-06 14:50:47 +0000

    Update comp_weight.py

--- python/fish_blocks/comp_weight.py
+++ python/fish_blocks/comp_weight.py
@@ -18,7 +18,7 @@ class comp_weight(gr.sync_block):
         gr.sync_block.__init__(self,
             name="comp_weight",
             in_sig=[(np.complex64, vector_len)] * len(mag),
-            out_sig=[(np.complex64, vector_len)]
+            out_sig=[(np.complex64, vector_len)] + len(mag) * [(np.complex64, vector_len)]
                                
         )
         
@@ -44,13 +44,19 @@ class comp_weight(gr.sync_block):
         
         mag, phase = self.parse_weights()
                         
-        phase_factors = np.exp(1j * np.radians(phase))
-        final_combined = np.sum(inputs_contiguous * mag[:, np.newaxis] * phase_factors[:, np.newaxis], axis=0).astype(np.complex64)
-        
+        phase_factors = np.exp(1j * np.radians(phase))      
+        weighted_signals = inputs_contiguous * mag[:, np.newaxis] * phase_factors[:, np.newaxis]
+        final_combined = np.sum(weighted_signals, axis=0).astype(np.complex64)
+
         final_combined_vectors = final_combined.reshape((n_vectors, self.vector_len))
         output_items[0][:n_vectors] = final_combined_vectors
         
+        weighted_signals_vectors = weighted_signals.reshape((self.in_num, n_vectors, self.vector_len))
+        for i in range(self.in_num):
+            output_items[i+1][:n_vectors] = weighted_signals_vectors[i]
+
         return n_vectors
+
 
     def parse_weights(self):
         mag = list(self.mag)
